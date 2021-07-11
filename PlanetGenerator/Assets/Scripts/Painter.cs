159c13e19ec01b3c859389b468ce94c98c476e4e
Савелий <savukhin@gmail.com>
2021-07-11 21:47:19 +0300

    Added Generating a flat mesh with noizing hills

---
+++ PlanetGenerator/Assets/Scripts/Painter.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Painter : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateShape();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1)  * (zSize + 1)];
+        
+        for (int z = 0, i = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.5f, z * 0.5f) * 4f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    void Update()
+    {
+        UpdateShape();
+    }
+
+    void UpdateShape() 
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+
+ /*   private void OnDrawGizmos()
+    {
+        for (int i = 0; i < vertices.Length / 2; i++)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+        for (int i = 0; i < vertices.Length / 2; i++)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(vertices[i + vertices.Length / 2], .1f);
+        }
+    }*/
+}
